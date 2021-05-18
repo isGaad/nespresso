3d46cef04edd123f0c13091d5603d8c68eac62f5
Islam <islaam.gaad@gmail.com>
2021-05-18 17:26:30 +0200

    array map to return only the sku

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var express     = require('express');
-var app         = express();
-var path        = require('path');
-var mongoose    = require('mongoose');
-var config      = require('./app/config');
-var Pod         = require('./app/models/Pod');
-var Machine     = require('./app/models/Machine');
+var express          = require('express');
+var app              = express();
+var path             = require('path');
+var mongoose         = require('mongoose');
+var config           = require('./app/config');
+var Pod              = require('./app/models/Pod');
+var Machine          = require('./app/models/Machine');
 var connectionString = `mongodb://${config.db.host}:${config.db.port}/${config.db.name}`;
 
 mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -15,20 +15,28 @@ app.get('/', function(request, response) {
 });
 
 app.get('/api/pods', function(request, response) {
-    Pod.find(request.query, function(err, pods) {
+    var query = Pod.find(request.query);
+
+    query.exec(function(err, pods) {
         if (err)
             responses.send(err);
+
+        pods = pods.map(pod => pod.sku);
 
         response.json(pods);
     });
 });
 
 app.get('/api/machines', function(request, response) {
-    Machine.find(request.query, function(err, pods) {
+    var query = Machine.find(request.query);
+
+    query.exec(request.query, function(err, machines) {
         if (err)
             responses.send(err);
 
-        response.json(pods);
+        machines = machines.map(machine => machine.sku);
+
+        response.json(machines);
     });
 });
 
